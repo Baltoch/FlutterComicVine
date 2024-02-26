e8747d3c7b8f1953e03b5a66f72e8140d3f17841
Paul Valerie GOMA <paulvalerie.goma@edu.ece.fr>
2024-02-26 11:47:10 +0100

    Adding assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class AppColors {
+  static const Color screenBackgroundColor = Color(0xFF15232E);
+  static const Color orange = Color(0xFFFF8100);
+  static const Color cardBackgroundColor = Color(0xFF1E3243);
+  static const Color cardElementBackgroundColor = Color(0xFF284C6A);
+  static const Color seeMoreBackgroundColor = Color(0xFF0F1921);
+  static const Color bottomBarBackgroundColor = Color(0xFF0F1E2B);
+  static const Color bottomBarBackgroundColorChecked = Color(0xFF12273C);
+  static const Color bottomBarTextColorChecked = Color(0xFF12273C);
+  static const Color bottomBarTextColorNonChecked = Color(0xFF778BA8);
 }
 
 class MyApp extends StatelessWidget {
