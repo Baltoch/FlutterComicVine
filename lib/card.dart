0639ee1612028daeddaa84abe0660b2af55ab2b8
Paul Valerie GOMA <paulvalerie.goma@edu.ece.fr>
2024-03-02 17:38:33 +0100

    Building card template

--- lib/card.dart
+++ lib/card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 enum CardType {
   movie,
@@ -7,16 +8,36 @@ enum CardType {
   character;
 }
 
-class SettingUpCard {
-  static const int cardWidth = 180;
-  static const int cardHeight = 242;
-  static const int cardBorderRadius = 10;
+class CardTemplate extends StatelessWidget {
+  const CardTemplate({super.key});
+
+  static const double cardWidth = 180;
+  static const double cardHeight = 242;
+  static const double cardBorderRadius = 10;
   static const Color cardBackgroundColor = Color(0xFF1E3243);
-  static const int imageWidth = 180;
-  static const int imageHeight = 177;
+  static const double imageWidth = 180;
+  static const double imageHeight = 177;
 
-  final Map<String, String> description;
-  final CardType type;
+  // final Map<String, String> description;
+  // final CardType type;
 
-  SettingUpCard({required this.description, required this.type});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: cardWidth,
+        height: cardHeight,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(cardBorderRadius)),
+          color: cardBackgroundColor,
+        ),
+        child: const Center(
+          child: Text(
+            'Salut',
+            style: TextStyle(fontFamily: 'Nunito', color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
 }
