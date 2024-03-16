fc71bea17500e1b7ad83c4fc546fba2769fa4fa1
Balthazar LEBRETON <balthazar.lebreton@gmail.com>
2024-03-16 12:41:16 +0100

    Tabs first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'appcolors.dart';
+import 'tabs.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        scaffoldBackgroundColor: AppColors.screenBackgroundColor, // Set the background color
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -88,34 +90,17 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: Tabs(
+              header: ["Histoire", "Personnages", "Episodes"], 
+              content: [
+                Text("Histoire"),
+                Text("Personnages"),
+                Text("Episodes")
+              ]
+            )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
---
+++ lib/tabs.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercomicvine/appcolors.dart';
+
+class Tabs extends StatelessWidget {
+  const Tabs({super.key, required this.header, required this.content});
+
+  final List<String> header;
+  final List<Widget> content;
+  
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: content.length, 
+      child: Column(
+        children: [
+          TabBar(
+            labelColor: Colors.white,
+            unselectedLabelColor: const Color.fromARGB(153, 255, 255, 255),
+            dividerColor: Colors.transparent,
+            indicatorColor: AppColors.orange,
+            overlayColor: const MaterialStatePropertyAll(Colors.transparent),
+            tabs: List<Tab>.generate(header.length, (i) => Tab(text: header[i]))
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: AppColors.cardBackgroundColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), 
+                  topRight: Radius.circular(20)
+                )
+              ),
+              child: TabBarView(
+                children: content
+              ),
+            )
+          )
+        ],
+      )
+    );
+  }
+}
