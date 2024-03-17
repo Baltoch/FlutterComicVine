e2ac051f74b0c8347f573d3f189d33750caa2452
Balthazar LEBRETON <balthazar.lebreton@gmail.com>
2024-03-17 12:02:19 +0100

    Update title

--- lib/title.dart
+++ lib/title.dart
@@ -8,15 +8,13 @@ class AppTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(
-        text: content,
-        style: const TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.w700,
-          color : Colors.white,
-          fontFamily: 'Nunito'
-        )
+    return Text(
+      content,
+      style: const TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.w700,
+        color : Colors.white,
+        fontFamily: 'Nunito'
       ),
     );
   }
