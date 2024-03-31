43b447765a02023d3a9b51eeb166f0a8c7880e11
Balthazar LEBRETON <balthazar.lebreton@gmail.com>
2024-03-31 20:27:22 +0200

    Adding listener to backbutton

--- lib/widget/backbutton.dart
+++ lib/widget/backbutton.dart
@@ -2,29 +2,36 @@ import 'package:flutter/material.dart';
 import '../utils/appcolors.dart';
 
 class MyBackButton extends StatelessWidget {
-  const MyBackButton({super.key, required this.title});
+  const MyBackButton({super.key, required this.title, required this.onClick});
 
   final String title;
+  final void Function(PointerDownEvent) onClick;
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      const Icon(
-        Icons.arrow_back_ios,
-        color: AppColors.white,
-        size: 24,
-      ),
-      Padding(
-          padding: const EdgeInsets.only(left: 21),
-          child: Text(
-            title,
-            style: const TextStyle(
+    return Listener(
+      onPointerDown: onClick,
+      child: Row(
+        children: [
+          const Icon(
+            Icons.arrow_back_ios,
+            color: AppColors.white,
+            size: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 21),
+            child: Text(
+              title,
+              style: const TextStyle(
                 color: AppColors.white,
                 fontSize: 20,
                 fontFamily: 'Nunito',
                 decoration: TextDecoration.none,
                 fontWeight: FontWeight.w700),
-          ))
-    ]);
+            )
+          )
+        ]
+      )
+    );
   }
 }
