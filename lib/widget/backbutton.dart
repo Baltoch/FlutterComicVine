c1382ebf0885d254808f50f06904a2e462654e4c
Paul Valerie GOMA <paulvalerie.goma@edu.ece.fr>
2024-03-31 20:20:04 +0200

    Update backbuttom

--- lib/widget/backbutton.dart
+++ lib/widget/backbutton.dart
@@ -2,41 +2,29 @@ import 'package:flutter/material.dart';
 import '../utils/appcolors.dart';
 
 class MyBackButton extends StatelessWidget {
-  const MyBackButton({super.key, required this.title, required this.onClick});
+  const MyBackButton({super.key, required this.title});
 
   final String title;
-  final Function(PointerDownEvent) onClick;
 
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: onClick,
-      child: Center(
-        child: Container(
-          height: 100,
-          width: 300,
-          decoration: const BoxDecoration(
-            color: Colors.transparent,
-          ),
-          child: Row(
-            children: [
-              const Icon(
-                Icons.arrow_back_ios,
-                color: Color.fromARGB(255, 255, 255, 255),
-                size: 20,
-              ),
-              Text(
-                title,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 20,
-                  fontFamily: 'Nunito',
-                ),
-              )
-            ]
-          )
-        )
-      )
-    );
+    return Row(children: [
+      const Icon(
+        Icons.arrow_back_ios,
+        color: AppColors.white,
+        size: 24,
+      ),
+      Padding(
+          padding: const EdgeInsets.only(left: 21),
+          child: Text(
+            title,
+            style: const TextStyle(
+                color: AppColors.white,
+                fontSize: 20,
+                fontFamily: 'Nunito',
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.w700),
+          ))
+    ]);
   }
 }
